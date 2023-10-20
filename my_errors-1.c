2d16582acca27f9ac818c83edb201e7dc09e702f
mosescode1 <emosescode@gmail.com>
2023-10-20 09:12:05 +0100

    all

--- my_errors-1.c
+++ my_errors-1.c
@@ -17,7 +17,16 @@ int my_erratoi(char *siz)
 	{
 		switch (siz[a])
 		{
-			case '0' ... '9':
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
 				check *= 10;
 				check += (siz[a] - '0');
 				if (check > INT_MAX)
